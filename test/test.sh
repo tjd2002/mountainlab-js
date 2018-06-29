ee34a0d46bf4b17b8c9c8a134fc7a1cab390e93a
Tom Davidson <tjd@alum.mit.edu>
2018-06-28 21:32:09 -0700

    fixup! fix Travis testing of local installs;  remove global install testing from npm test

--- test/test.sh
+++ test/test.sh
@@ -1,5 +1,5 @@
 #/bin/bash
 set -e -u
-export PATH=$INIT_CWD/bin:$PATH # $INIT_CWD provided by npm run-script
+export PATH="$INIT_CWD"/bin:"$PATH"; # $INIT_CWD provided by npm run-script
 ml-config
 ml-list-processors
