7c0b9a42fc739e3d45637ebcf70583859e6b9c92
Tom Davidson <tjd@alum.mit.edu>
2018-06-28 20:23:05 -0700

    fix Travis testing of local installs;  remove global install testing from npm test

--- test/test.sh
+++ test/test.sh
@@ -1,13 +1,5 @@
 #/bin/bash
-
-set -e
-if [ -z "$GLOBAL_INSTALL_FLAG" ]
-then
-    echo [testing a local install]
-    DIR=`dirname $0`
-    export PATH=$DIR/../bin:$PATH
-else
-    echo [ testing a global install, npm bin -g = $(npm bin -g) ]
-fi
-
+set -e -u
+export PATH=$INIT_CWD/bin:$PATH # $INIT_CWD provided by npm run-script
+ml-config
 ml-list-processors
