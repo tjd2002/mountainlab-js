ae6c9ddf2831ab0386c485456502c38f8f37741c
Tom Davidson <tjd@alum.mit.edu>
2018-06-29 22:52:05 -0700

    URGGG: Works great for a1-a2-a3-a4-d4-d3-d2-d1, but this is not what Conda does? a1-d1-a2-d2...

--- conda-recipe/mountainlab_activate.sh
+++ conda-recipe/mountainlab_activate.sh
@@ -1,11 +1,35 @@
-# Save previous state (use file in conda env to build a 'stack', env can be visited multiple times so key with CONDA_SHLVL)
-declare -x mldir_conda=$CONDA_PREFIX/etc/mountainlab 
-declare -x mldir_conda_$CONDA_SHLVL=$mldir_conda # save it for now
+#!/bin/bash -e
 
-declare -x saved_mlconfig_varname=ML_CONFIG_DIRECTORY_save_shlvl_$CONDA_SHLVL
+# bash notes:
+# - unlike 'export', 'declare -x' allows for variable expansion on left side of =
+# - ${!varname} is indirect expansion. if A=B, B=10, then ${!A} evaluates to 10
 
-# if ML_CONFIG_DIRECTORY exists, and we haven't already saved it, save it (uses indirect expansion)
-if [ ! -z $ML_CONFIG_DIRECTORY ] && [ -z ${!saved_mlconfig_varname} ]; then
-    declare -x $saved_mlconfig_varname=$ML_CONFIG_DIRECTORY
-    declare -x ML_CONFIG_DIRECTORY=$mldir_conda
+# This will be default mountainlab dir (equivalent to ~/.mountainlab) for this install
+mldir_conda="$CONDA_PREFIX"/etc/mountainlab 
+
+# save for debugging
+declare -x mldir_conda_"$CONDA_SHLVL"="$mldir_conda" # unlike 'export', declare -x allows for variable expansion
+
+# Name of var to save/load previous ML_CONFIG_DIRECTORY, keyed to $CONDA_SHLVL, 
+# which is unique to each level of the env stack
+saved_mlconfig_varname=ML_CONFIG_DIRECTORY_PREV_CONDA_SHLVL_"$CONDA_SHLVL"
+
+echo ===previous ML_CONFIG_DIRECTORY="$ML_CONFIG_DIRECTORY"
+echo ===CONDA_SHLVL="$CONDA_SHLVL"
+echo ===mldir_conda="$mldir_conda"
+echo ===saved_mlconfig_varname="$saved_mlconfig_varname"
+
+# if ML_CONFIG_DIRECTORY exists, we haven't already saved it (avoid overwriting on multiple activation), save it
+if [ -n "$ML_CONFIG_DIRECTORY" ] && [ -z "${!saved_mlconfig_varname}" ]; then
+    declare -x "$saved_mlconfig_varname"="$ML_CONFIG_DIRECTORY"
+    echo === Saving previous ML_CONFIG_DIRECTORY \( \'"${!saved_mlconfig_varname}"\' \) as var "$saved_mlconfig_varname"
+else
+    echo === not saving...
+    if [ -n "$ML_CONFIG_DIRECTORY" ]; then
+	echo     ... \'"$saved_mlconfig_varname"\' is non-empty \( set to \'"${!saved_mlconfig_varname}"\' \)
+    else
+	echo     ... ML_CONFIG_DIRECTORY tests empty.
+    fi
 fi
+echo === Setting ML_CONFIG_DIRECTORY to \'"$mldir_conda"\'
+export ML_CONFIG_DIRECTORY="$mldir_conda"
--- conda-recipe/mountainlab_deactivate.sh
+++ conda-recipe/mountainlab_deactivate.sh
@@ -1,9 +1,13 @@
-mldir_conda=$CONDA_PREFIX/etc/mountainlab 
+#!/bin/bash -e
 
-saved_mlconfig_varname=ML_CONFIG_DIRECTORY_save_shlvl_$CONDA_SHLVL
+# Name of var to save/load previous ML_CONFIG_DIRECTORY, keyed to $CONDA_SHLVL, 
+# which is unique to each level of the env stack
+saved_mlconfig_varname=ML_CONFIG_DIRECTORY_PREV_CONDA_SHLVL_"$CONDA_SHLVL"
 
-# if there's a saved ML_CONFIG_DIRECTORY, then recover it (uses indirect expansion)
-if  [ ! -z ${!saved_mlconfig_varname} ]; then
+# if there's a saved ML_CONFIG_DIRECTORY for this conda_shlvl (i.e. the current env),
+# then recover it (uses indirect expansion)
+if  [ -n "${!saved_mlconfig_varname}" ]; then
     export ML_CONFIG_DIRECTORY=${!saved_mlconfig_varname}
-    unset $saved_mlconfig_varname
+    unset $saved_mlconfig_varname # note indirection!
 fi
+unset mldir_conda_"$CONDA_SHLVL" # just here for debugging
