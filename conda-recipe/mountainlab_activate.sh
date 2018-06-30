7571c2807494826167b8d9d8115ce1dec9b42863
Tom Davidson <tjd@alum.mit.edu>
2018-06-30 01:01:45 -0700

    conda-activate: Much simpler approach, don't save any env vars. Overly verbose messages

--- conda-recipe/mountainlab_activate.sh
+++ conda-recipe/mountainlab_activate.sh
@@ -1,35 +1,24 @@
 #!/bin/bash -e
-
-# bash notes:
-# - unlike 'export', 'declare -x' allows for variable expansion on left side of =
-# - ${!varname} is indirect expansion. if A=B, B=10, then ${!A} evaluates to 10
+echo "=== [ mountainlab-js activate script ] ======================================="
+echo ":"
 
 # This will be default mountainlab dir (equivalent to ~/.mountainlab) for this install
-mldir_conda="$CONDA_PREFIX"/etc/mountainlab 
+mldir_conda="${CONDA_PREFIX?}"/etc/mountainlab
 
-# save for debugging
-declare -x mldir_conda_"$CONDA_SHLVL"="$mldir_conda" # unlike 'export', declare -x allows for variable expansion
-
-# Name of var to save/load previous ML_CONFIG_DIRECTORY, keyed to $CONDA_SHLVL, 
-# which is unique to each level of the env stack
-saved_mlconfig_varname=ML_CONFIG_DIRECTORY_PREV_CONDA_SHLVL_"$CONDA_SHLVL"
-
-echo ===previous ML_CONFIG_DIRECTORY="$ML_CONFIG_DIRECTORY"
-echo ===CONDA_SHLVL="$CONDA_SHLVL"
-echo ===mldir_conda="$mldir_conda"
-echo ===saved_mlconfig_varname="$saved_mlconfig_varname"
-
-# if ML_CONFIG_DIRECTORY exists, we haven't already saved it (avoid overwriting on multiple activation), save it
-if [ -n "$ML_CONFIG_DIRECTORY" ] && [ -z "${!saved_mlconfig_varname}" ]; then
-    declare -x "$saved_mlconfig_varname"="$ML_CONFIG_DIRECTORY"
-    echo === Saving previous ML_CONFIG_DIRECTORY \( \'"${!saved_mlconfig_varname}"\' \) as var "$saved_mlconfig_varname"
-else
-    echo === not saving...
-    if [ -n "$ML_CONFIG_DIRECTORY" ]; then
-	echo     ... \'"$saved_mlconfig_varname"\' is non-empty \( set to \'"${!saved_mlconfig_varname}"\' \)
+# if user has specified ML_CONFIG_DIRECTORY, warn, but don't overwrite it
+if [ -z "$ML_CONFIG_DIRECTORY" ]; then
+    echo ": OK! Default mountainlab search path for active env ${CONDA_PROMPT_MODIFIER}is:"
+    echo ":     '$mldir_conda'"
+    export ML_CONFIG_DIRECTORY=$mldir_conda
+    export MLCONFDIR_WASEMPTY=true
+else 
+    if [ -n "$MLCONFDIR_WASEMPTY" ] && [ "$ML_CONFIG_DIRECTORY" = "$mldir_conda" ]; then 
+	echo ": OK! Reactivation, nothing to do."
     else
-	echo     ... ML_CONFIG_DIRECTORY tests empty.
+	echo ": WARN! Default mountainlab search path already set ('$ML_CONFIG_DIRECTORY')."
+	echo ":       This may override conda search paths; run 'unset ML_CONFIG_DIRECTORY' to reset."
     fi
 fi
-echo === Setting ML_CONFIG_DIRECTORY to \'"$mldir_conda"\'
-export ML_CONFIG_DIRECTORY="$mldir_conda"
+echo ":"
+echo ": Run 'ml-config' to see all search paths in use."
+echo "==============================================================================="
--- conda-recipe/mountainlab_deactivate.sh
+++ conda-recipe/mountainlab_deactivate.sh
@@ -1,13 +1,19 @@
 #!/bin/bash -e
+echo "=== [ mountainlab-js deactivation ] ==========================================="
+echo ":"
+mldir_conda="${CONDA_PREFIX?}"/etc/mountainlab
 
-# Name of var to save/load previous ML_CONFIG_DIRECTORY, keyed to $CONDA_SHLVL, 
-# which is unique to each level of the env stack
-saved_mlconfig_varname=ML_CONFIG_DIRECTORY_PREV_CONDA_SHLVL_"$CONDA_SHLVL"
-
-# if there's a saved ML_CONFIG_DIRECTORY for this conda_shlvl (i.e. the current env),
-# then recover it (uses indirect expansion)
-if  [ -n "${!saved_mlconfig_varname}" ]; then
-    export ML_CONFIG_DIRECTORY=${!saved_mlconfig_varname}
-    unset $saved_mlconfig_varname # note indirection!
+# if we set confdir on activation, and it hasn't changed, unset it.
+if [ -n "$MLCONFDIR_WASEMPTY" ] && [ "$ML_CONFIG_DIRECTORY" = "$mldir_conda" ]; then
+    echo ": Deactivating mountainlab search path for env ${CONDA_PROMPT_MODIFIER}."
+    unset ML_CONFIG_DIRECTORY 
+    unset MLCONFDIR_WASEMPTY
+else 
+    if [ -n "$ML_CONFIG_DIRECTORY" ]; then
+	echo ": User-set ML_CONFIG_DIRECTORY left unchanged ( '$ML_CONFIG_DIRECTORY' )."
+    else
+	echo ": ML_CONFIG_DIRECTORY already unset/null, nothing to do"
+    fi
 fi
-unset mldir_conda_"$CONDA_SHLVL" # just here for debugging
+echo ":"
+echo "==============================================================================="
